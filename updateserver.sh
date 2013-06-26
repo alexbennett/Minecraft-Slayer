778de92afd7278a15a39ee5b5c9b28bdbfa1ae17
Alex Bennett <me@alexben.net>
2013-06-26 14:03:02 -0500

    This should do it.

--- updateserver.sh
+++ updateserver.sh
@@ -8,6 +8,6 @@ FOLDER="devserver"
 sudo cp -f /home/jenkins/jobs/Minecraft-Slayer/workspace/build/Slayer.jar /home/minecraft/$FOLDER/plugins/Slayer.jar
 
 # Reload server
-screen -S $SCREEN -p 0 -X stuff "`printf "/say Updating to the latest Slayer build!\r"`";
-screen -S $SCREEN -p 0 -X stuff "`printf "/reload\r"`";
-screen -S $SCREEN -p 0 -X stuff "`printf "/say Update complete.\r"`";
+sudo screen -S $SCREEN -p 0 -X stuff "`printf "/say Updating to the latest Slayer build!\r"`";
+sudo screen -S $SCREEN -p 0 -X stuff "`printf "/reload\r"`";
+sudo screen -S $SCREEN -p 0 -X stuff "`printf "/say Update complete.\r"`";
